8b0e84a85f24e5329702019b3deab885f44df4bd
tkalani <tanmay.k16@iiits.in>
2019-08-19 12:30:32 +0530

    New

---
+++ GeeksForGeeks/Dynamic Programming/12_optimal-strategy-for-game.cpp
@@ -0,0 +1,63 @@
+/*Problem statement :  Consider a row of n coins of values v1 . . . vn, where n is even. We play a game against an opponent by alternating turns. In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of the coin. Determine the maximum possible amount of money we can definitely win if we move first. 
+
+F(i, j)  represents the maximum value the user can collect from 
+         i'th coin to j'th coin.
+
+    F(i, j)  = Max(Vi + min(F(i+2, j), F(i+1, j-1) ), 
+                   Vj + min(F(i+1, j-1), F(i, j-2) )) 
+Base Cases
+    F(i, j)  = Vi           If j == i
+    F(i, j)  = max(Vi, Vj)  If j == i+1
+
+*/
+
+// C++ program to find out maximum value from a 
+// given sequence of coins 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Returns optimal value possible that a player can 
+// collect from an array of coins of size n. Note 
+// than n must be even 
+int optimalStrategyOfGame(int* arr, int n) 
+{ 
+	// Create a table to store solutions of subproblems 
+	int table[n][n]; 
+
+	// Fill table using above recursive formula. Note 
+	// that the table is filled in diagonal fashion (similar 
+	// to http:// goo.gl/PQqoS), from diagonal elements to 
+	// table[0][n-1] which is the result. 
+	for (int gap = 0; gap < n; ++gap) { 
+		for (int i = 0, j = gap; j < n; ++i, ++j) { 
+
+			// Here x is value of F(i+2, j), y is F(i+1, j-1) and 
+			// z is F(i, j-2) in above recursive formula 
+			int x = ((i + 2) <= j) ? table[i + 2][j] : 0; 
+			int y = ((i + 1) <= (j - 1)) ? table[i + 1][j - 1] : 0; 
+			int z = (i <= (j - 2)) ? table[i][j - 2] : 0; 
+
+			table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z)); 
+		} 
+	} 
+
+	return table[0][n - 1]; 
+} 
+
+// Driver program to test above function 
+int main() 
+{ 
+	int arr1[] = { 8, 15, 3, 7 }; 
+	int n = sizeof(arr1) / sizeof(arr1[0]); 
+	printf("%d\n", optimalStrategyOfGame(arr1, n)); 
+
+	int arr2[] = { 2, 2, 2, 2 }; 
+	n = sizeof(arr2) / sizeof(arr2[0]); 
+	printf("%d\n", optimalStrategyOfGame(arr2, n)); 
+
+	int arr3[] = { 20, 30, 2, 2, 2, 10 }; 
+	n = sizeof(arr3) / sizeof(arr3[0]); 
+	printf("%d\n", optimalStrategyOfGame(arr3, n)); 
+
+	return 0; 
+} 
