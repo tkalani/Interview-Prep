e37ea64c992bad930b6d9f1dbcdf7b27838bf590
tkalani <tanmay.k16@iiits.in>
2019-08-20 07:19:30 +0530

    New

---
+++ GeeksForGeeks/Arrays/31_arrange-given-numbers-form-biggest-number-set-2.py
@@ -0,0 +1,43 @@
+# Python program to find largest 
+# number from the given values 
+# function that return largest 
+# possible number 
+def largestNumber(array): 
+	
+	# extval is a empty list for extended 
+	# values and ans for calculating answer 
+	extval, ans = [], "" 
+	
+	# calculating the length of largest number 
+	# from given and add 1 for further operation 
+	l = len(str(max(array))) + 1
+	
+	# iterate given values and 
+	# calculating extended values 
+	for i in array: 
+		temp = str(i) * l 
+		
+		# make tuple of extended value and 
+		# equivalant original value then 
+		# append to list 
+		extval.append((temp[:l:], i)) 
+	
+	# sort extval in descending order 
+	extval.sort(reverse = True) 
+	
+	# iterate extended values 
+	for i in extval: 
+		
+		# concatinate original value equivalant 
+		# to extended value into ans variable 
+		ans += str(i[1]) 
+		
+	return ans 
+
+# Driver code 
+a = [1, 34, 3, 98, 9, 76, 
+		45, 4, 12, 121] 
+
+print(largestNumber(a)) 
+
+# This code is contributed by Chhekur 
